845fbe7179d796b9c939822efa2c10b4455cbcc1
ayushcodes0 <ayushsinghcodes@gmail.com>
2026-01-11 18:27:38 +0530

    fix: env injection

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 
 const app = require('./src/app');
 const connectDB = require('./src/config/db');
